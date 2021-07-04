4a1ecbb3333c5663b8270a62af8abb863b526468
Pavlo <pawel.wowniuk@gmail.com>
2021-07-04 21:44:17 +0200

    List all owners on Owners Index Page. Closes #16

--- pet-clinic-web/src/main/java/pavlo/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pavlo/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,7 +1,11 @@
 package pavlo.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pavlo.springframework.sfgpetclinic.services.OwnerService;
+
+import javax.jws.WebParam;
 
 /**
  * Created by Pavlo 14.03.2021
@@ -9,8 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
-    @RequestMapping({"","/", "/index", "/index.html"})
-    public String ListOwners(){
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String ListOwners(Model model) {
+model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
