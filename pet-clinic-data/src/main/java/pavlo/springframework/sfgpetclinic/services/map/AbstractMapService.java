e303d405dfac195785ca7dc80c44d34b97d06bd2
Pavlo <pawel.wowniuk@gmail.com>
2021-07-05 14:04:04 +0200

    Rafactored Map Impl abstract generattion. Closes #23

--- pet-clinic-data/src/main/java/pavlo/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/pavlo/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,14 +1,13 @@
 package pavlo.springframework.sfgpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
+import pavlo.springframework.sfgpetclinic.model.BaseEntity;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
 @Service
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -18,16 +17,33 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
-        return object;
+    T save( T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNexId());
+            }
+            map.put(object.getId(), object);
+        }else {
+            throw new RuntimeException("Object can not be null");
+        }
+    return object;
     }
 
-    void deleteByID(ID id) {
-        map.remove(id);
-    }
+        void deleteByID (ID id){
+            map.remove(id);
+        }
 
-    void delete(T object) {
-        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+        void delete (T object){
+            map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+        }
+
+        private Long getNexId() {
+           Long nextId = null;
+           try {
+               nextId= Collections.max(map.keySet()) + 1;
+           } catch (NoSuchElementException e){
+               nextId = 1L;
+           }
+           return nextId;
+        }
     }
-}
